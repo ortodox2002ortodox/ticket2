d21d14aa60c434fc74947e201ebe6220fd04381d
ortodox2002ortodox <58324913+ortodox2002ortodox@users.noreply.github.com>
2021-02-10 10:37:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class Main
+
+{
+
+	public static void Mai(string[] args)
+
+	{
+
+		
+
+		while (hasNext())
+
+		{
+
+			string boards = nextLine();
+
+			boards = " " + boards;
+
+			double[] c = new double[boards.Length];
+
+
+
+			c[0] = 0;
+
+			for (int i = 1; i < boards.Length; i++)
+
+			{
+
+				c[i] = 2000000000;
+
+				if (boards[i] == '.')
+				{
+
+					c[i] = c[i - 1];
+				}
+
+				else
+				{
+
+					for (int j = 0; j < i; j++)
+					{
+
+						c[i] = Math.Min(c[i], c[j] + Math.Sqrt(1.0 * i - j));
+					}
+				}
+
+			}
+
+
+
+			Console.WriteLine(c[boards.Length - 1]);
+
+		}
+
+	}
+
+    private static string nextLine()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static bool hasNext()
+    {
+        throw new NotImplementedException();
+    }
+}
